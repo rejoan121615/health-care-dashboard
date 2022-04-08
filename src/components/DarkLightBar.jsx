1469112af3d2ed36ff6d70211cc6516b7f8f1115
Mohd Rejoan <wearecrx@gmail.com>
2022-04-08 18:47:28 +0600

    dark light bar section complete

---
+++ src/components/DarkLightBar.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import Contrast from "../assets/navigation/Contrast.svg";
+import Switch from "react-switch";
+
+const DarkLightBar = (props) => {
+    const [checked, setChecked] = useState(false);
+
+    const handleChange = () => {
+        setChecked(!checked);
+    };
+
+    return (
+        <div className=" mt-6 flex justify-between items-center">
+            {/* left section  */}
+            <div>
+                <h6 className=" text-sm font-bold text-red-500 capitalize">
+                    welcome {props.name.split(" ")[1]},
+                </h6>
+                <p className=" capitalize text-[12px] mt-1">
+                    {" "}
+                    How're you feeling today?
+                </p>
+            </div>
+            {/* right section  */}
+            <div className=" flex items-center">
+                <div className=" w-5 h-5 ml-2">
+                    <img
+                        className=" w-full mt-1"
+                        src={Contrast}
+                        alt="contrast icon"
+                    />
+                </div>
+                <Switch
+                    id="switch"
+                    offColor="#e2e2f2"
+                    onColor="#f5dee5"
+                    offHandleColor="#343434"
+                    onHandleColor="#f80d38"
+                    uncheckedIcon={false}
+                    checkedIcon={false}
+                    onChange={handleChange}
+                    checked={checked}
+                    className=" scale-50 mt-2"
+                />
+                <p>Apply dark theme</p>
+            </div>
+        </div>
+    );
+};
+
+export default DarkLightBar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import PortalNavBar from '../components/PortalNavBar'
-import SearchBar from '../ui/SearchBar'
+import SearchBar from '../components/SearchBar'
+import DarkLightBar from '../components/DarkLightBar'
 
 
 const Home = () => {
+
+  const userData = {
+    name: 'Mohd Rejoan',
+    
+  }
+
+
   return (
     <section>
       <SearchBar />
+      <DarkLightBar name={userData.name} />
     </section>
   )
 }
